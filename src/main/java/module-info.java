c5906dcc75da4fea7ac1f72270ae46331a2ba3b1
ultra <64664984+ultrasin@users.noreply.github.com>
2022-08-24 17:46:23 -0600

    restore (#2)
    * Paquetes service, model y (util + AppContex)
    
    * orden de login
    
    cambio en el login ui/ux final solo necesita un estilo en css.
    
    Co-authored-by: Arauz <86898345+kjarj54@users.noreply.github.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module cr.ac.una.pz.cancha {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    requires java.logging;
+    
     opens cr.ac.una.pz.cancha to javafx.fxml;
     exports cr.ac.una.pz.cancha;
 }
