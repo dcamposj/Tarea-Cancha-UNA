1e5ff6c35d544ca0ad83e0794f5567d61eec9ecb
ultra <64664984+ultrasin@users.noreply.github.com>
2022-08-25 18:43:33 -0600

    google maps
    se añadio el google maps en el fxml menuprincipal.fxml para acomodarlo luego

--- src/main/java/cr/ac/una/pz/cancha/MenuController.java
+++ src/main/java/cr/ac/una/pz/cancha/MenuController.java
@@ -4,9 +4,14 @@
  */
 package cr.ac.una.pz.cancha;
 
-import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import java.net.URL;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 /**
  * FXML Controller class
@@ -15,12 +20,25 @@ import javafx.fxml.Initializable;
  */
 public class MenuController implements Initializable {
 
+    @FXML
+    private WebView webView;
+    @FXML
+    private Button btnCargar;
+    
+    private WebEngine engine;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        engine = webView.getEngine();
+        engine.load("https://www.google.es/maps/?hl=es");
     }    
+
+    @FXML
+    private void cargarContenido(ActionEvent event) {
+        
+    }
     
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module cr.ac.una.pz.cancha {
     requires javafx.fxml;
     requires java.base;
     requires java.logging;
+    requires javafx.web;
     
     opens cr.ac.una.pz.cancha to javafx.fxml;
     exports cr.ac.una.pz.cancha;
